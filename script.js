54898e2d2d6c6c3e62e6a62fc13fe8cf1e235a7f
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2024-09-10 12:28:40 -0600

    feat: funcion que maneja el input nombre

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log ("Conteo de vocales");
+/**
+ Encuentra el número de letras que existen en el
+ nombre
+ @letra {string} letra a buscar
+ @nombre {string} nombre a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+
+function manejoDelEventoDeInputNombre(){
+	console.log("Me estoy invocando");
+}
